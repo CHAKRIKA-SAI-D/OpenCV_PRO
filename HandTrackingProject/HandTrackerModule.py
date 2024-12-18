f0b6e33421f6807b32151fb1ed088a8f91b34f3b
CHAKRIKA-SAI-D <22wh1a1299@bvrithyderabad.edu.in>
2024-12-18 11:43:07 +0530

    new commit

---
+++ HandTrackingProject/HandTrackerModule.py
@@ -0,0 +1,63 @@
+import cv2
+import mediapipe as mp
+import time
+
+class HandDetector():
+    def __init__(self,mode=False,maxHands=2,detectionCon=0.5,trackCon=0.5):
+        self.mode=mode
+        self.maxHands=maxHands
+        self.detectionCon=detectionCon
+        self.trackCon=trackCon
+        self.mpHands=mp.solutions.hands
+        self.hands = self.mpHands.Hands(static_image_mode=self.mode, 
+                                max_num_hands=self.maxHands, 
+                                min_detection_confidence=self.detectionCon, 
+                                min_tracking_confidence=self.trackCon)
+        self.mpDraw=mp.solutions.drawing_utils
+    def findHands(self,img,draw=True):
+        #the below only uses the rgb images so we need to change
+        imgRGB=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+        self.results=self.hands.process(imgRGB)
+        # print(results.multi_hand_landmarks)
+        if self.results.multi_hand_landmarks:
+            for handLms in self.results.multi_hand_landmarks:
+                if draw:
+                    self.mpDraw.draw_landmarks(img,handLms,self.mpHands.HAND_CONNECTIONS)
+                #handconnections is for drawing lines for each points
+        return img
+    def findPosition(self,img,handNo=0,draw=True):
+        lmlist=[]
+        if self.results.multi_hand_landmarks:
+            myHand=self.results.multi_hand_landmarks[handNo]
+            for id,lm in enumerate(myHand.landmark):
+                    # print(id,lm)
+                    h,w,c=img.shape
+                    cx,cy=int(lm.x*w),int(lm.y*h) #to knw the exact position in pixels
+                    # print(id,cx,cy)
+                    lmlist.append([id,cx,cy])
+                    if draw:
+                        cv2.circle(img,(cx,cy),15,(255,0,255),cv2.FILLED)
+        return lmlist
+def main():
+    pTime=0
+    cTime=0
+    cap=cv2.VideoCapture(0)
+    detector=HandDetector()
+    
+    while True:
+        success,img=cap.read()
+        img=detector.findHands(img)
+        lmlist=detector.findPosition(img)
+        if lmlist:
+            print(lmlist[4]) #4=>tip of thumb
+        cTime=time.time() 
+        fps=1/(cTime-pTime)
+        pTime=cTime
+        cv2.putText(img,str(int(fps)),(10,70),cv2.FONT_HERSHEY_COMPLEX,3,(255,0,255),3)
+
+        cv2.imshow("Image",img)
+        if cv2.waitKey(1) & 0xFF==ord(" "):
+            break
+
+if  __name__=="__main__":
+    main()
